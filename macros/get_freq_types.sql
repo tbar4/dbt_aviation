df8997d5469c1327ebe92eb51a95dfe2cacf6b54
tbar4 <trevor.barnes91@gmail.com>
2021-04-16 10:51:46 -0700

    changed pivot logic to capture top US freqs at large airports

--- macros/get_freq_types.sql
+++ macros/get_freq_types.sql
@@ -1,23 +1,24 @@
 {% macro get_freq_types() %}
 
 {% set freq_type_query %}
-select distinct
-type
+select
+UPPER(REGEXP_REPLACE(type, '/', '_')) as type
 from {{ source('aviation', 'airport_frequencies') }}
+where substring(airport_ident, 1, 1) = 'K'
 group by 1
-
+having COUNT(*) > 100
+order by 1
 {% endset %}
 
 {% set results = run_query(freq_type_query) %}
 
 {% if execute %}
+{# Return the first column #}
 {% set results_list = results.columns[0].values() %}
 {% else %}
 {% set results_list = [] %}
 {% endif %}
 
-{{ log(results, info=True) }}
-
-{{ return([]) }}
+{{ return(results_list) }}
 
 {% endmacro %}
--- models/intermediate/list_top_freq__pivot.sql
+++ models/intermediate/list_top_freq__pivot.sql
@@ -2,10 +2,17 @@
 {%- set freq_types = get_freq_types() -%}
 
 select
-airport_ident,
+af.airport_ident,
 {%- for freq_type in freq_types %}
-case when type = '{{freq_type}}' then frequency_mhz end as {{freq_type}}_amount
+MAX(
+  case when UPPER(REGEXP_REPLACE(af.type, '/', '_')) = '{{freq_type}}'
+  then frequency_mhz
+  end) as {{freq_type}}_freq
 {%- if not loop.last %},{% endif -%}
 {% endfor %}
-from {{ source('aviation', 'airport_frequencies') }}
-group by 1
+from {{ source('aviation', 'airport_frequencies') }} af
+
+INNER JOIN {{ ref('stg_us_large_airports') }} sula
+  ON af.airport_ident = sula.ident
+
+GROUP BY 1
